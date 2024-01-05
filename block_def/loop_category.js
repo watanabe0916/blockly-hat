f686cb53795abfa62983af6e2a169421c43a9e66
Y-Takeru <f20200@e.hiroshima-cu.ac.jp>
2024-01-05 09:21:12 +0900

    looo

--- block_def/loop_category.js
+++ block_def/loop_category.js
@@ -269,9 +269,8 @@ Blockly.Hat.loop_if_break = function (block) {
     order += 'if (' + conditions + ' ) break\n';
     order += '(';
     order += syori + '\n';
-    order += ') ^()\n';
-    order += 'loop' + ' ' + temp_args + '. break )';
-    order += ' ' + temp_inits + '^() \n';
+    order += 'loop' + ' ' + temp_args + '. break ) ^()\n';
+    order += ') ' + temp_inits + '^() \n';
 
     return order;
 };
